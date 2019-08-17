c2485c851a26a9c0c2da18e480e7339ace538804
Nathan Loveless <nathansl2003@yahoo.com>
2019-08-16 23:41:37 -0400

    Completed all MVP and part of stretch goals.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,41 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker1
+{
+	constructor(attr)
+	{
+		this.length = attr.length;
+		this.width = attr.width;
+		this.height = attr.height;
+	}
+	
+	volume() { return this.length * this.width * this.height; }
+	surfaceArea() { return 2 * (this.length * this.width + this.length * this.width + this.width * this.height); }
+
+}
+
+// Stretch Goal
+class CubeMaker extends CuboidMaker1
+{
+  constructor(attr)
+  {
+    super(attr);
+  }
+
+  volume() { return this.length * this.width * this.height; }
+  surfaceArea() { return 6 * Math.pow(this.length, 2); }
+
+
+}
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+
+const cuboid1 = new CuboidMaker1({ 'length': 4, 'width': 5, 'height': 5});
+ console.log(cuboid1.volume()); // 100
+ console.log(cuboid1.surfaceArea()); // 130
+
+// Stretch goal, remember, a cube has equal sides, we only need one of these, to find the surface area
+const cube = new CubeMaker({ 'length': 5, 'width': 5, 'height': 5})
+ console.log(cube.volume());
+ console.log(cube.surfaceArea());
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
